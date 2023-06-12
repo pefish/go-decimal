6a3c7dbed51fadab43c492563e730e80a271f0b9
pefish <darrondarron@qq.com>
2023-06-12 11:58:02 +0800

    v0.3.0

--- decimal.go
+++ decimal.go
@@ -161,6 +161,10 @@ func (decimalInstance *DecimalClass) EndForUint64() uint64 {
 	return go_reflect.Reflect.MustToUint64(decimalInstance.result.String())
 }
 
+func (decimalInstance *DecimalClass) EndForInt64() int64 {
+	return go_reflect.Reflect.MustToInt64(decimalInstance.result.String())
+}
+
 // 直接截取
 func (decimalInstance *DecimalClass) TruncForString(precision int32) string {
 	return decimalInstance.Trunc(precision).result.String()
